0ea7f80172143eb50d3df21f9479dbe8d7d05789
Duncan Champney <DChampney@wareto.com>
2020-11-16 19:51:22 -0500

    Initial Commit

---
+++ CountdownTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CountdownTimer
+//
+//  Created by Duncan Champney on 11/16/20.
+//  Copyright © 2020 Duncan Champney. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CountdownTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CountdownTimer
+//
+//  Created by Duncan Champney on 11/16/20.
+//  Copyright © 2020 Duncan Champney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
